5f3c822e899a6857638785f5f1d8cd4c0cde7440
Denis Abdullabekov <mrden@inbox.ru>
2020-09-26 19:56:53 +1000

    31 - WEB API & Implementing API Versioning

--- UserService/IUserSvc.cs
+++ UserService/IUserSvc.cs
@@ -1,4 +1,5 @@
-﻿using ModelService;
+﻿using Microsoft.AspNetCore.Http;
+using ModelService;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +10,12 @@ namespace UserService
     public interface IUserSvc
     {
         Task<ProfileModel> GetUserProfileByIdAsync(string userId);
+        Task<ProfileModel> GetUserProfileByUsernameAsync(string username);
+        Task<ProfileModel> GetUserProfileByEmailAsync(string email);
+        Task<bool> CheckPasswordAsync(ProfileModel model, string password);
+        Task<bool> UpdateProfileAsync(IFormCollection formData);
+        Task<bool> AddUserActivity(ActivityModel model);
+        Task<bool> ChangePasswordAsync(ProfileModel model, string newPassword);
+        Task<List<ActivityModel>> GetUserActivity(string username);
     }
 }
--- UserService/UserSvc.cs
+++ UserService/UserSvc.cs
@@ -12,6 +12,7 @@ using ModelService;
 using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -106,6 +107,205 @@ namespace UserService
             return userProfile;
         }
 
+        public async Task<ProfileModel> GetUserProfileByUsernameAsync(string username)
+        {
+            var userProfile = new ProfileModel();
+
+            try
+            {
+                var loggedInUserId = GetLoggedInUserId();
+                var user = await _userManager.FindByIdAsync(loggedInUserId);
+                if (user == null || user.UserName != username) return null;
+
+                userProfile = await GetUserProfileByIdAsync(user.Id);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred in GetUserProfileByUsernameAsync {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
+
+            return userProfile;
+        }
+
+        public async Task<ProfileModel> GetUserProfileByEmailAsync(string email)
+        {
+            var userProfile = new ProfileModel();
+
+            try
+            {
+                var loggedInUserId = GetLoggedInUserId();
+                var user = await _userManager.FindByIdAsync(loggedInUserId);
+                if (user == null || user.Email != email) return null;
+
+                userProfile = await GetUserProfileByIdAsync(user.Id);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred in GetUserProfileByUsernameAsync {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
+
+            return userProfile;
+        }
+
+        public async Task<bool> CheckPasswordAsync(ProfileModel model, string password)
+        {
+            try
+            {
+                var loggedUserId = GetLoggedInUserId();
+                var user = await _userManager.FindByIdAsync(loggedUserId);
+
+                if (user.UserName != _cookieSvc.Get("username") ||
+                    user.UserName != model.Username)
+                {
+                    return false;
+                }                    
+
+                if (!await _userManager.CheckPasswordAsync(user, password))
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred in CheckPasswordAsync {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
+            return true;
+        }
+
+        public async Task<bool> UpdateProfileAsync(IFormCollection formData)
+        {
+            var loggedInUserId = GetLoggedInUserId();
+            var user = await _userManager.FindByIdAsync(loggedInUserId);
+
+            if (user == null) return false;
+
+            if (user.UserName != _cookieSvc.Get("username") ||
+                user.UserName != formData["username"].ToString() ||
+                user.Email != formData["email"].ToString())
+                return false;
+
+            try
+            {
+                ActivityModel activityModel = new ActivityModel { UserId = user.Id };
+                await UpdateProfilePicAsync(formData, user);
+
+                user.Firstname = formData["firstname"];
+                user.Birthday = formData["birthdate"];
+                user.Lastname = formData["lastname"];
+                user.Middlename = formData["middlename"];
+                user.DisplayName = formData["displayname"];
+                user.PhoneNumber = formData["phone"];
+                user.Gender = formData["gender"];
+                user.TwoFactorEnabled = Convert.ToBoolean(formData["IsTwoFactorOn"]);
+
+                /* If Addresses exist we update them => If Addresses do not exist we add them */
+                await InsertOrUpdateAddress(user.Id, "Shipping", formData["saddress1"], formData["saddress2"], formData["scountry"], formData["sstate"], formData["scity"], formData["spostalcode"], formData["sunit"]);
+                await InsertOrUpdateAddress(user.Id, "Billing", formData["address1"], formData["address2"], formData["country"], formData["state"], formData["city"], formData["postalcode"], formData["unit"]);
+
+                await _userManager.UpdateAsync(user);
+
+                activityModel.Date = DateTime.UtcNow;
+                activityModel.IpAddress = _cookieSvc.GetUserIP();
+                activityModel.Location = _cookieSvc.GetUserCountry();
+                activityModel.OperatingSystem = _cookieSvc.GetUserOS();
+                activityModel.Type = "Profile update successful";
+                activityModel.Icon = "fas fa-thumbs-up";
+                activityModel.Color = "success";
+                await _activitySvc.AddUserActivity(activityModel);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred while updating profile {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
+            return false;
+        }
+
+
+        public async Task<bool> AddUserActivity(ActivityModel model)
+        {
+            try
+            {
+                await _activitySvc.AddUserActivity(model);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred while seeding the database  {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
+
+            return false;
+        }
+
+        public async Task<bool> ChangePasswordAsync(ProfileModel model, string newPassword)
+        {
+            bool result;
+            try
+            {
+                ActivityModel activityModel = new ActivityModel();
+                activityModel.Date = DateTime.UtcNow;
+                activityModel.IpAddress = _cookieSvc.GetUserIP();
+                activityModel.Location = _cookieSvc.GetUserCountry();
+                activityModel.OperatingSystem = _cookieSvc.GetUserOS();
+
+                var loggedInUserId = GetLoggedInUserId();
+                var user = await _userManager.FindByIdAsync(loggedInUserId);
+
+                if (user != null)
+                {
+                    user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, newPassword);
+                    var updateResult = await _userManager.UpdateAsync(user);
+                    result = updateResult.Succeeded;
+                    activityModel.UserId = user.Id;
+                    activityModel.Type = "Password Changed successful";
+                    activityModel.Icon = "fas fa-thumbs-up";
+                    activityModel.Color = "success";
+                    await _activitySvc.AddUserActivity(activityModel);
+                }
+                else
+                {
+                    result = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                Log.Error("An error occurred while seeding the database  {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
+
+            return result;
+        }
+
+        public async Task<List<ActivityModel>> GetUserActivity(string username)
+        {
+            List<ActivityModel> userActivities = new List<ActivityModel>();
+
+            try
+            {
+                var loggedInUserId = GetLoggedInUserId();
+                var user = await _userManager.FindByIdAsync(loggedInUserId);
+
+                if (user == null || user.UserName != username) return null;
+
+                userActivities = await _db.Activities.Where(x => x.UserId == user.Id).OrderByDescending(o => o.Date).Take(20).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred while seeding the database  {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
+
+            return userActivities;
+        }
+
         private string GetLoggedInUserId()
         {
             try
@@ -121,6 +321,86 @@ namespace UserService
                     ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
             }
             return null;
+        }
+
+        private async Task<ApplicationUser> UpdateProfilePicAsync(IFormCollection formData, ApplicationUser user)
+        {
+            // First we create an empty array to store old file info
+            var oldProfilePic = new string[1];
+            // we will store path of old file to delete in an empty array.
+            oldProfilePic[0] = Path.Combine(_env.WebRootPath + user.ProfilePic);
+
+            // Create the Profile Image Path
+            var profPicPath = _env.WebRootPath + $"{Path.DirectorySeparatorChar}uploads{Path.DirectorySeparatorChar}user{Path.DirectorySeparatorChar}profile{Path.DirectorySeparatorChar}";
+
+            // If we have received any files for update, then we update the file path after saving to server
+            // else we return the user without any changes
+            if (formData.Files.Count <= 0) return user;
+
+            var extension = Path.GetExtension(formData.Files[0].FileName);
+            var filename = DateTime.Now.ToString("yymmssfff");
+            var path = Path.Combine(profPicPath, filename) + extension;
+            var dbImagePath = Path.Combine($"{Path.DirectorySeparatorChar}uploads{Path.DirectorySeparatorChar}user{Path.DirectorySeparatorChar}profile{Path.DirectorySeparatorChar}", filename) + extension;
+
+            user.ProfilePic = dbImagePath;
+
+            // Copying New Files to the Server - profile Folder
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await formData.Files[0].CopyToAsync(stream);
+            }
+
+            // Delete old file after successful update
+            if (!File.Exists(oldProfilePic[0])) return user;
+
+            File.SetAttributes(oldProfilePic[0], FileAttributes.Normal);
+            File.Delete(oldProfilePic[0]);
+
+            return user;
+        }
+
+        private async Task InsertOrUpdateAddress(string userId, string type, string line1, string line2, string country,
+            string state, string city, string postalcode, string unit)
+        {
+            var updateAddress = _db.Addresses.FirstOrDefault(ad => ad.User.Id == userId && ad.Type == type);
+            await using var dbContextTransaction = await _db.Database.BeginTransactionAsync();
+            try
+            {
+                var newAddress = new AddressModel();
+                if (updateAddress != null)
+                {
+                    updateAddress.Line1 = line1;
+                    updateAddress.Line2 = line2;
+                    updateAddress.Country = country;
+                    updateAddress.City = city;
+                    updateAddress.State = state;
+                    updateAddress.PostalCode = postalcode;
+                    updateAddress.Unit = unit;
+                    _db.Entry(updateAddress).State = EntityState.Modified;
+                }
+                else
+                {
+                    newAddress.Line1 = line1;
+                    newAddress.Line2 = line2;
+                    newAddress.Country = country;
+                    newAddress.City = city;
+                    newAddress.State = state;
+                    newAddress.PostalCode = postalcode;
+                    newAddress.Unit = unit;
+                    _db.Entry(newAddress).State = EntityState.Added;
+                }
+
+                await _db.SaveChangesAsync();
+
+                await dbContextTransaction.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                await dbContextTransaction.RollbackAsync();
+
+                Log.Error("An error occurred while seeding the database  {Error} {StackTrace} {InnerException} {Source}",
+                    ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+            }
         }
     }
 }
