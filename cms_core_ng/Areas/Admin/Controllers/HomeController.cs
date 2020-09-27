eb7899469e1619af9696c5898b72f575f3c6e172
Denis Abdullabekov <mrden@inbox.ru>
2020-09-27 18:34:25 +1000

    34 - HomeController | Change Password | Update Profile | Display Activity - Admin User

--- cms_core_ng/Areas/Admin/Controllers/HomeController.cs
+++ cms_core_ng/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using CookieService;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using ModelService;
+using UserService;
+using WritableOptionsService;
+using System;
 
 namespace CMS_CORE_NG.Areas.Admin.Controllers
 {
@@ -11,9 +16,42 @@ namespace CMS_CORE_NG.Areas.Admin.Controllers
     [Authorize(AuthenticationSchemes ="Admin")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserSvc _userSvc;
+        private readonly ICookieSvc _cookieSvc;
+        private readonly IServiceProvider _provider;
+        private readonly DataProtectionKeys _dataProtectionKeys;
+        private readonly AppSettings _appSettings;
+        private readonly IWritableSvc<SiteWideSettings> _writableSiteWideSettings;
+        private AdminBaseViewModel _adminBaseViewModel;
+
+        public HomeController(IUserSvc userSvc, ICookieSvc cookieSvc,
+            IServiceProvider provider,
+            IOptions<DataProtectionKeys> dataProtectionKeys, IOptions<AppSettings> appSettings,
+            IWritableSvc<SiteWideSettings> writableSiteWideSettings)
         {
-            return View();
+            _userSvc = userSvc;
+            _cookieSvc = cookieSvc;
+            _provider = provider;
+            _dataProtectionKeys = dataProtectionKeys.Value;
+            _appSettings = appSettings.Value;
+            _writableSiteWideSettings = writableSiteWideSettings;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var protectorProvider = _provider.GetService<IDataProtectionProvider>();
+            var protector = protectorProvider.CreateProtector(_dataProtectionKeys.ApplicationUserKey);
+            var userProfile = await _userSvc.GetUserProfileByIdAsync(protector.Unprotect(_cookieSvc.Get("user_id")));
+            var addUserModel = new AddUserModel();
+
+            _adminBaseViewModel = new AdminBaseViewModel
+            {
+                Profile = userProfile,
+                AddUser = addUserModel,
+                AppSetting = _appSettings,
+                SiteWideSetting = _writableSiteWideSettings.Value
+            };
+            return View("Index", _adminBaseViewModel);
         }
     }
 }
